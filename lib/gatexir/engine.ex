54e8ef09f3e81960b79c96432cffcbe4df6a2dc2
Jefferson Farias <jefferson.farias7@gmail.com>
2022-07-31 21:27:50 -0300

    handle call in engine

--- lib/gatexir/engine.ex
+++ lib/gatexir/engine.ex
@@ -3,24 +3,41 @@ defmodule Gatexir.Engine do
 
   alias Plug.Conn
 
+  @impl true
   def init(_opts) do
-    config = Gatexir.Config.load("config.yml")
-
-    {:ok, config: config}
+    with {:ok, config} <- Gatexir.Config.load("config.yml") do
+      {:ok, config: config}
+    else
+      _ -> raise "Invalid or missing configuration gateway file"
+    end
   end
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
+  @impl true
+  def handle_call({:route, _path_info}, _from, config: config) do
+    request = nil
+
+    # here will match whitch url will route
+
+    {:reply, request, config: config}
+  end
+
   # here we will make http request to proceed based on config e return it for other module dispatch the given request
   def route(%Conn{} = conn) do
-    %Conn{method: method, path_info: path_info} = conn |> IO.inspect()
+    %Conn{method: _method, path_info: path_info} =
+      conn
+      |> IO.inspect()
 
     [_ | path] = path_info
 
     IO.inspect(path, label: "path")
 
-    :ok
+    case GenServer.call(__MODULE__, {:route, path}) do
+      nil -> {:error, :no_matched}
+      to -> to
+    end
   end
 end
