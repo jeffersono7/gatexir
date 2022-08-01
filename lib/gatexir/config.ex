366277cf761fb6dea318d3556b60190505fe10e4
Jefferson Farias <jefferson.farias7@gmail.com>
2022-07-31 21:09:31 -0300

    parse config

--- lib/gatexir/config.ex
+++ lib/gatexir/config.ex
@@ -1,8 +1,24 @@
 defmodule Gatexir.Config do
+  defstruct [:mappings]
 
   def load(filename) do
     File.cwd!()
     |> Path.join(["mapping/", filename])
     |> YamlElixir.read_from_file()
+    |> parse()
   end
+
+  defp parse({:ok, %{"mappings" => mappings} = config_map}) when is_map(config_map) do
+    struct =
+      for {service, config} <- mappings, reduce: %{} do
+        acc ->
+          default = %{match: config["match"], to: config["to"]}
+
+          Map.update(acc, String.to_atom(service), default, fn _ -> config end)
+      end
+
+    {:ok, struct}
+  end
+
+  defp parse(params), do: params
 end
